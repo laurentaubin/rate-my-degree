bbc6a9622de8653109f8d74332feca150eb8c88d
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-03-28 12:41:23 -0400

    Basic home page layout

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Input } from "@chakra-ui/input";
-import { useCoursesQuery } from "../generated/graphql";
+import { HomeHeader } from "../components/HomeHeader";
 
 const Index = () => {
-  const [{ data }] = useCoursesQuery();
-  console.log(data);
-
-  return <Input placeholder="Search a course"></Input>;
+  return (
+    <>
+      <HomeHeader />
+      <Input placeholder="Search a course"></Input>{" "}
+    </>
+  );
 };
 export default Index;
