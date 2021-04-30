946d0c482b14f3b6136a0b37fd3af26455dbfa3d
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-04-30 10:41:28 -0400

    Redirect to 404 page when trying to access a non existing course

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,14 @@
+import { Text } from "@chakra-ui/layout";
+import React from "react";
+import { NavBar } from "../components/NavBar";
+
+const Index = () => {
+  return (
+    <>
+      <NavBar />
+      <Text>404 page</Text>
+    </>
+  );
+};
+
+export default Index;
