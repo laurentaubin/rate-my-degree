ed34473bfa439ac36ea586b35a24d6779913c01c
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-04-30 10:30:59 -0400

    change some stuff in docker compose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,23 @@ import cors from "cors";
 import express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
-import { Course } from "./entities/Course";
-import { CourseComment } from "./entities/CourseComment";
 import { CommentResolver } from "./resolvers/CommentResolver";
 import { CourseResolver } from "./resolvers/CourseResolver";
 
 const main = async () => {
+  console.log(process.env.DB_HOST);
+
   await createConnection({
     type: "postgres",
-    database: "ratemyglo",
+    host: "0.0.0.0",
+    port: 3030,
     username: "postgres",
     password: "postgres",
+    database: "ratemyglo",
+    entities: ["dist/entities/*.js"],
+    migrations: ["dist/migrations/*.js"],
     logging: true,
     synchronize: true,
-    entities: [Course, CourseComment],
   });
 
   const app = express();
