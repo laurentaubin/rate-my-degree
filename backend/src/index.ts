3dfaeaf9ee3c7165354ae56cc9d87f44b2180795
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-04-30 23:18:22 -0400

    Revert "test path add log"
    This reverts commit 549bec3469e79097c90f4dcf8e9e2c1a47c4b4a6.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,6 @@ import { CommentResolver } from "./resolvers/CommentResolver";
 import { CourseResolver } from "./resolvers/CourseResolver";
 
 const main = async () => {
-  console.log("hello");
   await createConnection({
     type: "postgres",
     host: process.env.DB_HOST || "localhost",
