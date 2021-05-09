0c48b418d0ef1aabd598bbb98cf8dcf6532bc4af
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-05-09 08:51:02 -0400

    Use svg components instead of images for blobs

--- frontend/src/components/HomeHeader.tsx
+++ frontend/src/components/HomeHeader.tsx
@@ -4,25 +4,25 @@ import { GoogleLogin } from "@components/authentication/GoogleLogin";
 import FacebookIcon from "@components/icons/FacebookIcon";
 import WebsiteIcon from "@components/icons/WebsiteIcon";
 import React from "react";
+import { HomePageBlob } from "./blobs/HomePageBlob";
 
 interface HomeHeaderProps {}
 
 export const HomeHeader: React.FC<HomeHeaderProps> = ({}) => {
   return (
-    <Flex
-      height="20vh"
-      paddingTop="4vh"
-      backgroundImage="url(https://svgur.com/i/VYt.svg)"
-      backgroundRepeat="no-repeat"
-      backgroundPosition="60vw"
-    >
-      <Link href="https://www.facebook.com/groups/aeglo" isExternal marginRight="16px" marginLeft="15vw" height="20px">
-        <FacebookIcon boxSize={5} color="black" />
-      </Link>
-      <Link href="http://aeglo.ift.ulaval.ca/" isExternal height="20px">
-        <WebsiteIcon boxSize={5} color="black" />
-      </Link>
-      <GoogleLogin marginLeft="auto" marginRight="25vw" backgroundColor="black" color="white" _hover={{}} />
-    </Flex>
+    <>
+      <Flex width="100%" zIndex={-1} position="absolute">
+        <HomePageBlob marginLeft="auto" marginRight={[0, 0, 0, "15vw"]} />
+      </Flex>
+      <Flex height="20vh" paddingTop="4vh">
+        <Link href="https://www.facebook.com/groups/aeglo" isExternal marginRight="16px" marginLeft="15vw" height="20px">
+          <FacebookIcon boxSize={5} color="black" />
+        </Link>
+        <Link href="http://aeglo.ift.ulaval.ca/" isExternal height="20px">
+          <WebsiteIcon boxSize={5} color="black" />
+        </Link>
+        <GoogleLogin marginLeft="auto" marginRight={[5, 5, "25vw"]} backgroundColor="black" color="white" _hover={{}} />
+      </Flex>
+    </>
   );
 };
